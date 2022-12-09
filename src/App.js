23631fc56597e325d4197ac3bbcd8d1db6d872e2
unknown <md40revel@gmail.com>
2022-12-09 13:58:28 +0100

    creation de AddTuto2 et suppression de AddTuto

--- src/App.js
+++ src/App.js
@@ -3,24 +3,41 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import AddTuto from "./composants/AddTuto";
+import Tuto from "./composants/Tuto";
+//import TutorialsList from "./composants/TutorialsList";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <a href="/tutorials" className="navbar-brand">
+        bezKoder
+      </a>
+      <div className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <Link to={"/tutorials"} className="nav-link">
+            Tutorials
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/add"} className="nav-link">
+            Add
+          </Link>
+        </li>
+      </div>
+    </nav>
+
+    <div className="container mt-3">
+      <Routes>
+        {/*<Route path="/" element={<TutorialsList/>} />*/}
+        {/*<Route path="/tutorials" element={<TutorialsList/>} />*/}
+        <Route path="/" element={<AddTuto/>} />
+        <Route path="/tuto/:id" element={<Tuto/>} />
+      </Routes>
     </div>
+  </div>
   );
 }
 
