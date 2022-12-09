a852eaaf3f4b273b838cd695bf666fe36616b048
unknown <md40revel@gmail.com>
2022-12-09 14:44:26 +0100

    création de TutoList

---
+++ src/composants/TutoList.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import TutorialDataService from "../services/TutorialService";
+import { Link } from "react-router-dom";
+
+const TutoList = () => {
+  const [tutorials, setTutorials] = useState([]);
+  const [currentTutorial, setCurrentTutorial] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  useEffect(() => {
+    retrieveTutorials();
+  }, []);
+
+  const onChangeSearchTitle = e => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+  };
+
+  const retrieveTutorials = () => {
+    TutorialDataService.getAll()
+      .then(response => {
+        setTutorials(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const refreshList = () => {
+    retrieveTutorials();
+    setCurrentTutorial(null);
+    setCurrentIndex(-1);
+  };
+
+  const setActiveTutorial = (tutorial, index) => {
+    setCurrentTutorial(tutorial);
+    setCurrentIndex(index);
+  };
+
+  const removeAllTutorials = () => {
+    TutorialDataService.removeAll()
+      .then(response => {
+        console.log(response.data);
+        refreshList();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const findByTitle = () => {
+    TutorialDataService.findByTitle(searchTitle)
+      .then(response => {
+        setTutorials(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  return (
+    // ...
+  );
+};
+
+export default TutoList;
