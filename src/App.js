f88e5458d2ba54b1a5b3798ec66061a20732099e
unknown <md40revel@gmail.com>
2022-12-10 17:28:28 +0100

    ajout style css dans App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
